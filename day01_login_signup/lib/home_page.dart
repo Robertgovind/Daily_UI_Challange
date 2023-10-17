23475b53e12be55b42d95bd0bd4061058f72866c
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-17 07:45:53 +0545

    1st day

--- day01_login_signup/lib/home_page.dart
+++ day01_login_signup/lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:day01_login_signup/login.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,9 +6,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Row(
-        children: [Center(child: Text("Home Page"))],
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 33, 88, 241),
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage("assets/home.webp"),
+          ),
+          Text(
+            "Wherever the art of Medicine is loved, there is also a love of Humanity",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              LogIn();
+            },
+            child: Text("Get Started"),
+          ),
+        ],
       ),
     );
   }
