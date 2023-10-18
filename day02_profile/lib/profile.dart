1760cb5ae48e5ae03beb9c0373bcc5f4b81d9986
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-18 17:55:48 +0545

    2nd day

--- day02_profile/lib/profile.dart
+++ day02_profile/lib/profile.dart
@@ -6,7 +6,7 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Profile"),
+      body: Text("Profile "),
     );
   }
 }
