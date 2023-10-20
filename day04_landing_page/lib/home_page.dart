b6ee21e77c56ebeef9fd15ec1f4d417849f198b5
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-20 19:47:32 +0545

    4th day

--- day04_landing_page/lib/home_page.dart
+++ day04_landing_page/lib/home_page.dart
@@ -6,9 +6,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        bottomNavigationBar: Row(
+        bottomNavigationBar: const Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Icon(
@@ -33,6 +33,208 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+        body: Stack(
+          children: [
+            Positioned(
+              top: 135,
+              right: 10,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.pink)),
+                onPressed: () {},
+                child: const Text(
+                  "Popular",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.black),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.apps_rounded),
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage("assets/image1.webp"),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Text(
+                      "Hi,Emily",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Container(
+                      height: 40,
+                      child: Image(image: AssetImage("assets/emoji.webp")),
+                    )
+                  ],
+                ),
+                const Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "Welcome back",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ImageContainer(
+                        image: "assets/women1.webp",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ImageContainer(
+                        image: "assets/women2.webp",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ImageContainer(
+                        image: "assets/women3.webp",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ImageContainer(
+                        image: "assets/women4.webp",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                ),
+                const Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "Categories",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ContanerList(
+                        image: "assets/gifts.webp",
+                        maintext: "Gifts",
+                        subtext: "+60 articles",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ContanerList(
+                        image: "assets/clothes.webp",
+                        maintext: "Clothes",
+                        subtext: "+500 articles",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ContanerList(
+                        image: "assets/football.webp",
+                        maintext: "Sports",
+                        subtext: "+100 articles",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ContanerList(
+                        image: "assets/electronics.webp",
+                        maintext: "Electronics",
+                        subtext: "+60 articles",
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ImageContainer extends StatelessWidget {
+  const ImageContainer({
+    super.key,
+    required this.image,
+  });
+  final image;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 260,
+      width: 230,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+class ContanerList extends StatelessWidget {
+  const ContanerList({
+    super.key,
+    required this.image,
+    required this.maintext,
+    required this.subtext,
+  });
+  final image;
+  final maintext;
+  final subtext;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage(image),
+            radius: 35,
+          ),
+          Text(
+            maintext,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(subtext),
+        ],
       ),
     );
   }
