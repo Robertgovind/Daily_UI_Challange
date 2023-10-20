8e7627fe22691faf0ecb5aba7bbb83d8aa4d8368
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-20 06:13:51 +0545

    4th day

--- day04_landing_page/lib/home_page.dart
+++ day04_landing_page/lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return const SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              Icons.home,
+              size: 35,
+            ),
+            Icon(
+              Icons.search,
+              size: 35,
+            ),
+            Icon(
+              Icons.shopping_cart,
+              size: 35,
+            ),
+            Icon(
+              CupertinoIcons.heart_fill,
+              size: 35,
+            ),
+            Icon(
+              Icons.settings,
+              size: 35,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
