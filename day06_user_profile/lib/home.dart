25478430e13120c68a1dbb5900e4d3b1d7175782
Govind Kumar Yadav <robertgovind@gmail.com>
2023-10-22 17:07:15 +0545

    6th day

--- day06_user_profile/lib/home.dart
+++ day06_user_profile/lib/home.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                color: const Color.fromARGB(201, 242, 182, 2),
+                color: const Color.fromARGB(200, 157, 209, 237),
                 height: 200,
                 width: double.infinity,
               ),
@@ -53,37 +53,69 @@ class _HomeState extends State<Home> {
                           ),
                         ],
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Phone",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Text(
-                            "+5999-661-6161",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(left: 25, right: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Phone",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              "+5999-661-6161",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
                       SizedBox(
                         height: 10,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Mail",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Text(
-                            "marin@gmail.com",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(left: 25, right: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Mail",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              "marin@gmail.com",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Divider(),
+                      BottomCard(
+                        leadIcon: Icons.dark_mode,
+                        text: "Dark mode",
+                        lagIcon: Icons.circle,
+                        isFirst: true,
+                      ),
+                      Divider(),
+                      BottomCard(
+                        leadIcon: Icons.credit_card,
+                        text: "Cards",
+                      ),
+                      Divider(),
+                      BottomCard(
+                        leadIcon: Icons.person,
+                        text: "Profile details",
+                      ),
+                      Divider(),
+                      BottomCard(
+                        leadIcon: Icons.settings,
+                        text: "Settings",
+                      ),
+                      Divider(),
                     ],
                   ),
                 ),
@@ -95,10 +127,46 @@ class _HomeState extends State<Home> {
             top: 140,
             child: const CircleAvatar(
               radius: 60,
+              backgroundImage: AssetImage("assets/profile.jpg"),
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class BottomCard extends StatelessWidget {
+  const BottomCard(
+      {super.key,
+      required this.leadIcon,
+      required this.text,
+      this.lagIcon = Icons.arrow_forward_ios_outlined,
+      this.isFirst = false});
+  final leadIcon;
+  final text;
+  final lagIcon;
+  final isFirst;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(leadIcon),
+      title: Text(
+        text,
+        style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+      ),
+      hoverColor: const Color.fromARGB(255, 187, 147, 147),
+      trailing: isFirst
+          ? const Image(
+              height: 35,
+              image: AssetImage(
+                "assets/icon.webp",
+              ),
+            )
+          : Icon(
+              lagIcon,
+              color: Colors.black,
+            ),
     );
   }
 }
